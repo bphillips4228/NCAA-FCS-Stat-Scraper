5f7d8164e1a27663cabf583f0e37b3bebf148479
bphillips4228 <bphillips6@stu.jsu.edu>
2019-03-28 22:19:51 -0500

    v.3
    Tweaked the code to gather more data more efficiently
    Automatically gathers data from multiple seasons and weeks

--- scraper_espn.py
+++ scraper_espn.py
@@ -5,7 +5,18 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 import csv
 
+
+team_ids = {}
+season = 2015
+season_range = [2015, 2016, 2017, 2018]
+week_range = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 'Bowls']
 games_stats = []
+
+stat_categories = ['Season', 'WId', 'WScore', 'LId', 'LScore', 'WLoc', 'WFirst_Downs', 'WThird_Downs', 'WFourth_Downs', 'WTotal_Yards', 'WPassing',
+'WComp_Perc', 'WYards_Per_Pass', 'WInterceptions_Thrown', 'WRushing', 'WRushing_Attempts', 'WYards_Per_Rush', 'WPenalities', 'WPenality_Yards',
+'WTurnovers', 'WFumbles_Lost', 'WInterceptions_Thrown', 'WPossession','LFirst_Downs', 'LThird_Downs', 'LFourth_Downs', 'LTotal_Yards', 'LPassing',
+'LComp_Perc', 'LYards_Per_Pass', 'LInterceptions_Thrown', 'LRushing', 'LRushing_Attempts', 'LYards_Per_Rush', 'LPenalities', 'LPenality_Yards',
+'LTurnovers', 'LFumbles_Lost', 'LInterceptions_Thrown', 'LPossession']
 
 def find_percentage(data):
 	try:
@@ -55,276 +66,150 @@ def set_loser_stats(data, where):
 
 	return loser_data
 
+def get_id(data):
+	data = data.split('/')
+	id = data[5]
+	return id
+
+def collect_id(team, id):
+	try:
+		team_ids[team] = id
+	except:
+		pass
+
+def dict_to_list(dict):
+	dict_list = []
+	for i, j in dict.iteritems():
+		dict_list.append([i, j])
+	return dict_list
+
 def set_game_stats(game):
-	while True:
-		try:
-			game_stats = []
-			#team 1 is away... team 2 is home
-			team_1 = game.find('tr', {'class': 'away'})
-			team_1_name = team_1.find('span', {'class': 'sb-team-short'}).text.strip()
-			team_1_score = team_1.find('td', {'class': 'total'}).find('span').text.strip()
+	game_stats = []
+	#team 1 is away... team 2 is home
+	team_1 = game.find('tr', {'class': 'away'})
+	try:
+		team_1_id = get_id(team_1.find('a', {'name': '&lpos=college-football:scoreboard:team'})['href'])
+	except:
+		team_1_id  = 0
+	team_1_name = team_1.find('span', {'class': 'sb-team-short'}).text.strip()
+	try:
+		team_1_score = team_1.find('td', {'class': 'total'}).find('span').text.strip()
+	except:
+		pass
 
-			team_2 = game.find('tr', {'class': 'home'})
-			team_2_name = team_2.find('span', {'class': 'sb-team-short'}).text.strip()
-			team_2_score = team_2.find('td', {'class': 'total'}).find('span').text.strip()
+	team_2 = game.find('tr', {'class': 'home'})
+	try:
+		team_2_id = get_id(team_2.find('a', {'name': '&lpos=college-football:scoreboard:team'})['href'])
+	except:
+		team_2_id = 0
+	team_2_name = team_2.find('span', {'class': 'sb-team-short'}).text.strip()
+	try:
+		team_2_score = team_2.find('td', {'class': 'total'}).find('span').text.strip()
+	except:
+		pass
 
-			game_id = game['id']
-			game_url = f'http://www.espn.com/college-football/matchup?gameId={game_id}'
+	collect_id(team_1_name, team_1_id)
+	collect_id(team_2_name, team_2_id)
 
-			game_browser = webdriver.Chrome('C:/ChromeDriver/chromedriver.exe', options=options)
-			print(f'Getting stats from {team_1_name} vs. {team_2_name} game...')
-			game_browser.get(game_url)
+	game_id = game['id']
+	game_url = f'http://www.espn.com/college-football/matchup?gameId={game_id}'
 
-			game_html = game_browser.page_source
-			game_soup = BeautifulSoup(game_html, 'html.parser')
+	game_browser = webdriver.Chrome('C:/ChromeDriver/chromedriver.exe', options=options)
+	game_browser.get(game_url)
 
-			game_data = ((game_soup.find('table', {'class': 'mod-data'})).find('tbody')).findAll('tr')
+	game_html = game_browser.page_source
+	game_soup = BeautifulSoup(game_html, 'html.parser')
 
-			if int(team_2_score) > int(team_1_score):
-				game_stats.append(team_2_name)
-				game_stats.append(team_2_score)
-				game_stats.append(team_1_name)
-				game_stats.append(team_1_score)
-				game_stats.append('home')
-				game_stats.extend(set_winner_stats(game_data, 'away'))
-				game_stats.extend(set_loser_stats(game_data, 'home'))
+	print(f'Getting stats from {team_1_name} vs. {team_2_name} game...')
 
-			else:
-				game_stats.append(team_1_name)
-				game_stats.append(team_1_score)
-				game_stats.append(team_2_name)
-				game_stats.append(team_2_score)
-				game_stats.append('away')
-				game_stats.extend(set_winner_stats(game_data, 'home'))
-				game_stats.extend(set_loser_stats(game_data, 'away'))
+	try:
+		game_data = ((game_soup.find('table', {'class': 'mod-data'})).find('tbody')).findAll('tr')
 
-			game_browser.quit()
-
-			game_stats[6] = find_percentage(game_stats[6])
-			game_stats[7] = find_percentage(game_stats[7])
-			game_stats[10] = find_percentage(game_stats[10])
-			game_stats.insert(17, split_second(game_stats[16]))
-			game_stats[16] = split_first(game_stats[16])
-			game_stats[21] = convert_time(game_stats[21])
-			game_stats[23] = find_percentage(game_stats[23])
-			game_stats[24] = find_percentage(game_stats[24])
-			game_stats[27] = find_percentage(game_stats[27])
-			game_stats.insert(34, split_second(game_stats[33]))
-			game_stats[33] = split_first(game_stats[33])
-			game_stats[38] = convert_time(game_stats[38])
-
-
-			return game_stats
-
-		except:
-			continue
+		if int(team_2_score) > int(team_1_score):
+			game_stats.append(season)
+			game_stats.append(team_2_id)
+			game_stats.append(team_2_score)
+			game_stats.append(team_1_id)
+			game_stats.append(team_1_score)
+			game_stats.append(1) #1 equals home
+			game_stats.extend(set_winner_stats(game_data, 'away'))
+			game_stats.extend(set_loser_stats(game_data, 'home'))
 
 		else:
-			break
+			game_stats.append(season)
+			game_stats.append(team_1_id)
+			game_stats.append(team_1_score)
+			game_stats.append(team_2_id)
+			game_stats.append(team_2_score)
+			game_stats.append(0) #0 equals away
+			game_stats.extend(set_winner_stats(game_data, 'home'))
+			game_stats.extend(set_loser_stats(game_data, 'away'))
+
+		game_browser.quit()
+
+		game_stats[7] = find_percentage(game_stats[7])
+		game_stats[8] = find_percentage(game_stats[8])
+		game_stats[11] = find_percentage(game_stats[11])
+		game_stats.insert(18, split_second(game_stats[17]))
+		game_stats[17] = split_first(game_stats[17])
+		try:
+			game_stats[22] = convert_time(game_stats[22])
+		except:
+			game_stats[22] = 0
+		game_stats[24] = find_percentage(game_stats[24])
+		game_stats[25] = find_percentage(game_stats[25])
+		game_stats[28] = find_percentage(game_stats[28])
+		game_stats.insert(35, split_second(game_stats[34]))
+		game_stats[34] = split_first(game_stats[34])
+		try:
+			game_stats[39] = convert_time(game_stats[39])
+		except:
+			game_stats[39] = 0
+
+		return game_stats
+
+	except:
+		pass
 
 options = Options()
 options.add_argument('--headless')
 options.add_argument('log-level=3');
 
-page_url = 'http://www.espn.com/college-football/scoreboard/_/group/81/year/2013/seasontype/2/week/1'
+for season in season_range:
+	season = season
+	print('Season: ' + str(season))
+	for week in week_range:
+		print('Week: ' + str(week))
+		if week == 'Bowl':
+			page_url = f'http://www.espn.com/college-football/scoreboard/_/group/81/year/{season}/seasontype/3/week/1'
+		else:
+			page_url = f'http://www.espn.com/college-football/scoreboard/_/group/81/year/{season}/seasontype/2/week/{week}'
 
-browser = webdriver.Chrome('C:/ChromeDriver/chromedriver.exe', options=options)
-browser.get(page_url)
+		browser = webdriver.Chrome('C:/ChromeDriver/chromedriver.exe', options=options)
+		browser.get(page_url)
 
-html = browser.page_source
-soup = BeautifulSoup(html, 'html.parser')
+		html = browser.page_source
+		soup = BeautifulSoup(html, 'html.parser')
 
-games = soup.findAll('article', {'class': 'scoreboard football final home-winner js-show'})
+		games = soup.findAll('article', {'class': 'scoreboard'})
 
-for game in games:
+		for game in games:
 
-	game_data = set_game_stats(game)
-	if game_data != None:
-		print(game_data)
-		games_stats.append(game_data)
+			game_data = set_game_stats(game)
+			if game_data != None:
+				games_stats.append(game_data)
 
-	print(games_stats)
+			browser.quit()
 
-	browser.quit()
+with open('fcs_data_2015_2018.csv', 'w', newline='') as f:
+	writer = csv.writer(f)
+	writer.writerow(stat_categories)
+	writer.writerows(games_stats)
 
-	'''first_downs = game_data.find('tr', {'data-stat-attr': 'firstDowns'})
+with open('fcs_team_ids.csv', 'w', newline='') as f:
+	writer = csv.writer(f)
+	writer.writerow('TeamName', 'TeamID')
+	writer.writerows(dict_to_list(team_ids))
 
-	if winner == team_1_name:
-		winner_first_downs = first_downs.findAll('td')[1].text.strip()
-		loser_first_downs = first_downs.findAll('td')[2].text.strip()
-	else:
-		winner_first_downs = first_downs.findAll('td')[2].text.strip()
-		loser_first_downs = first_downs.findAll('td')[1].text.strip()
-
-	print('Winner First Downs: ' + str(winner_first_downs))
-	print('Loser First Downs: ' + str(winner_first_downs))
-
-	third_downs = game_data.find('tr', {'data-stat-attr': 'thirdDownEff'})
-
-	if winner == team_1_name:
-		winner_third_downs = find_percentage(third_downs.findAll('td')[1].text.strip())
-		loser_third_downs = find_percentage(third_downs.findAll('td')[2].text.strip())
-	else:
-		winner_third_downs = find_percentage(third_downs.findAll('td')[2].text.strip())
-		loser_third_downs = find_percentage(third_downs.findAll('td')[1].text.strip())
-
-	print('Winner Third Downs: ' + str(winner_third_downs))
-	print('Loser Third Downs: ' + str(loser_third_downs))
-
-	fourth_downs = game_data.find('tr', {'data-stat-attr': 'fourthDownEff'})
-
-	if winner == team_1_name:
-		winner_fourth_downs = find_percentage(fourth_downs.findAll('td')[1].text.strip())
-		loser_fourth_downs= find_percentage(fourth_downs.findAll('td')[2].text.strip())
-	else:
-		winner_fourth_downs = find_percentage(fourth_downs.findAll('td')[2].text.strip())
-		loser_fourth_downs = find_percentage(fourth_downs.findAll('td')[1].text.strip())
-
-	print('Winner Fourth Downs: ' + str(winner_fourth_downs))
-	print('Loser Fourth Downs: ' + str(loser_fourth_downs))
-
-	total_yards = game_data.find('tr', {'data-stat-attr': 'totalYards'})
-
-	if winner == team_1_name:
-		winner_total_yards = total_yards.findAll('td')[1].text.strip()
-		loser_total_yards= total_yards.findAll('td')[2].text.strip()
-	else:
-		winner_total_yards = total_yards.findAll('td')[2].text.strip()
-		loser_total_yards = total_yards.findAll('td')[1].text.strip()
-
-	print('Winner Total Yards: ' + str(winner_total_yards))
-	print('Loser Total Yards: ' + str(loser_total_yards))
-
-	passing = game_data.find('tr', {'data-stat-attr': 'netPassingYards'})
-
-	if winner == team_1_name:
-		winner_passing = passing.findAll('td')[1].text.strip()
-		loser_passing= passing.findAll('td')[2].text.strip()
-	else:
-		winner_passing = passing.findAll('td')[2].text.strip()
-		loser_passing = passing.findAll('td')[1].text.strip()
-
-	print('Winner Passing Yards: ' + str(winner_passing))
-	print('Loser Passing Yards: ' + str(loser_passing))
-
-	comp_percentage = game_data.find('tr', {'data-stat-attr': 'completionAttempts'})
-
-	if winner == team_1_name:
-		winner_comp_percentage = find_percentage(comp_percentage.findAll('td')[1].text.strip())
-		loser_comp_percentage= find_percentage(comp_percentage.findAll('td')[2].text.strip())
-	else:
-		winner_comp_percentage = find_percentage(comp_percentage.findAll('td')[2].text.strip())
-		loser_comp_percentage = find_percentage(comp_percentage.findAll('td')[1].text.strip())
-
-	print('Winner Completion Percentage: ' + str(winner_comp_percentage))
-	print('Loser Completion Percentage: ' + str(loser_comp_percentage))
-
-	passing_yards_per = game_data.find('tr', {'data-stat-attr': 'yardsPerPass'})
-
-	if winner == team_1_name:
-		winner_passing_yards_per = passing_yards_per.findAll('td')[1].text.strip()
-		loser_passing_yards_per= passing_yards_per.findAll('td')[2].text.strip()
-	else:
-		winner_passing_yards_per = passing_yards_per.findAll('td')[2].text.strip()
-		loser_passing_yards_per = passing_yards_per.findAll('td')[1].text.strip()
-
-	print('Winner Passing Yards Per Attempt: ' + str(winner_passing_yards_per))
-	print('Loser Passing Yards Per Attempt: ' + str(loser_passing_yards_per))
-
-	interceptions = game_data.find('tr', {'data-stat-attr': 'interceptions'})
-
-	if winner == team_1_name:
-		winner_interceptions = interceptions.findAll('td')[1].text.strip()
-		loser_interceptions= interceptions.findAll('td')[2].text.strip()
-	else:
-		winner_interceptions = interceptions.findAll('td')[2].text.strip()
-		loser_interceptions = interceptions.findAll('td')[1].text.strip()
-
-	print('Winner Interceptions Thrown: ' + str(winner_interceptions))
-	print('Loser Interceptions Thrown: ' + str(loser_interceptions))
-
-	rushing = game_data.find('tr', {'data-stat-attr': 'rushingYards'})
-
-	if winner == team_1_name:
-		winner_rushing = rushing.findAll('td')[1].text.strip()
-		loser_rushing= rushing.findAll('td')[2].text.strip()
-	else:
-		winner_rushing = rushing.findAll('td')[2].text.strip()
-		loser_rushing = rushing.findAll('td')[1].text.strip()
-
-	print('Winner Rushing Yards: ' + str(winner_rushing))
-	print('Loser Rushing Yards: ' + str(loser_rushing))
-
-	rush_attempts = game_data.find('tr', {'data-stat-attr': 'rushingAttempts'})
-
-	if winner == team_1_name:
-		winner_rush_attempts = rush_attempts.findAll('td')[1].text.strip()
-		loser_rush_attempts= rush_attempts.findAll('td')[2].text.strip()
-	else:
-		winner_rush_attempts = rush_attempts.findAll('td')[2].text.strip()
-		loser_rush_attempts = rush_attempts.findAll('td')[1].text.strip()
-
-	print('Winner Rush Attempts: ' + str(winner_rush_attempts))
-	print('Loser Rush Attempts: ' + str(loser_rush_attempts))
-
-	rush_yards_per = game_data.find('tr', {'data-stat-attr': 'yardsPerRushAttempt'})
-
-	if winner == team_1_name:
-		winner_rush_yards_per = rush_yards_per.findAll('td')[1].text.strip()
-		loser_rush_yards_per= rush_yards_per.findAll('td')[2].text.strip()
-	else:
-		winner_rush_yards_per = rush_yards_per.findAll('td')[2].text.strip()
-		loser_rush_yards_per = rush_yards_per.findAll('td')[1].text.strip()
-
-	print('Winner Rush Yard Per Attempt: ' + str(winner_rush_yards_per))
-	print('Loser Rush Yard Per Attempt: ' + str(loser_rush_yards_per))
-
-	penalties = game_data.find('tr', {'data-stat-attr': 'totalPenaltiesYards'})
-
-	if winner == team_1_name:
-		winner_penalties = split_first(penalties.findAll('td')[1].text.strip())
-		loser_penalties= split_first(penalties.findAll('td')[2].text.strip())
-	else:
-		winner_penalties = split_first(penalties.findAll('td')[2].text.strip())
-		loser_penalties = split_first(penalties.findAll('td')[1].text.strip())
-
-	print('Winner Penalties: ' + str(winner_penalties))
-	print('Loser Penalties: ' + str(loser_penalties))
-
-	penalty_yards = game_data.find('tr', {'data-stat-attr': 'totalPenaltiesYards'})
-
-	if winner == team_1_name:
-		winner_penalty_yards = split_second(penalty_yards.findAll('td')[1].text.strip())
-		loser_penalty_yards= split_second(penalty_yards.findAll('td')[2].text.strip())
-	else:
-		winner_penalty_yards = split_second(penalty_yards.findAll('td')[2].text.strip())
-		loser_penalty_yards = split_second(penalty_yards.findAll('td')[1].text.strip())
-
-	print('Winner Penalty Yards: ' + str(winner_penalty_yards))
-	print('Loser Penalty Yards: ' + str(loser_penalty_yards))
-
-	fumbles = game_data.find('tr', {'data-stat-attr': 'fumblesLost'})
-
-	if winner == team_1_name:
-		winner_fumbles = fumbles.findAll('td')[1].text.strip()
-		loser_fumbles= fumbles.findAll('td')[2].text.strip()
-	else:
-		winner_fumbles = fumbles.findAll('td')[2].text.strip()
-		loser_fumbles = fumbles.findAll('td')[1].text.strip()
-
-	print('Winner Fumbles Lost: ' + str(winner_fumbles))
-	print('Loser Fumbles Lost: ' + str(loser_fumbles))
-
-	possession = game_data.find('tr', {'data-stat-attr': 'possessionTime'})
-
-	if winner == team_1_name:
-		winner_possession = convertTime(possession.findAll('td')[1].text.strip())
-		loser_possession= convertTime(possession.findAll('td')[2].text.strip())
-	else:
-		winner_possession = convertTime(possession.findAll('td')[2].text.strip())
-		loser_possession = convertTime(possession.findAll('td')[1].text.strip())
-
-	print('Winner Posession: ' + str(winner_possession))
-	print('Loser Possession: ' + str(loser_possession))'''
 
 
